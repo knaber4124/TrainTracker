5052a540bba9c4ef94aaf6895c4011582daf0401
Keith Naber <knaber4124@gmail.com>
2019-08-06 22:28:57 -0400

    getting basic time logic in place

--- trainTracker.js
+++ trainTracker.js
@@ -1,26 +1,81 @@
 $(document).ready(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyAWQ3rY6DPVXZJHc_CDVH3mW6a2ka7psNM",
+        authDomain: "traintracker-482ca.firebaseapp.com",
+        databaseURL: "https://traintracker-482ca.firebaseio.com",
+        projectId: "traintracker-482ca",
+        storageBucket: "",
+        messagingSenderId: "44644774961",
+        appId: "1:44644774961:web:fb75e960592f73ff"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    database = firebase.database();
+    // let now = moment();
+    function update() {
+        $('.clock').html(moment().format('MMMM Do YYYY, h:mm a'));
+    }
+    setInterval(update, 1000);
     
 
-
-    $('.submit').on('click',function(event){
+    $('.submit').on('click', function (event) {
         event.preventDefault();
-        var newTrain=$('.nameInput').val().trim();
-        var newTrainDestination=$('.destinationInput').val().trim();
-        var newTrainFirstArrival=$('.arrivalInput').val().trim();
-        var newTrainInterval=$('.intervalInput').val().trim();
+
+        var newTrain = $('.nameInput').val().trim();
+        var newTrainDestination = $('.destinationInput').val().trim();
+        var newTrainFirstArrival = $('.arrivalInput').val().trim();
+        var newTrainInterval = $('.intervalInput').val().trim();
+        var firstArrivalconverted=moment(newTrainFirstArrival,'HH:mm').subtract(1,'years');
+        console.log(firstArrivalconverted);
+        var currentTime=moment();
+        var diffCalc=moment().diff(moment(firstArrivalconverted),'minutes');
+        var timeDifference= diffCalc % newTrainInterval;
+        var timeRemaining= newTrainInterval - timeDifference;
+        console.log(timeRemaining);
+        var nextArrival=moment().add(timeRemaining,'minutes').format('h:mma');
+        console.log(nextArrival);
         
-        let newTR=$('<tr>');
-        let trainName=$('<td>').text(newTrain);
-        let trainDesination=$('<td>').text(newTrainDestination);
-        let trainFrequency=$('<td>').text(newTrainInterval);
-        let nextArrivalTime=$('<td>').text(newTrainFirstArrival);
-        let nextArrivalInterval=$('<td>');
-        newTR.append(trainName,trainDesination,trainFrequency,nextArrivalTime,nextArrivalInterval);
-        $('.trainInfo').append(newTR);
-
-    });
 
 
 
+        // localStorage.setItem("newTrain", newTrain);
+        // localStorage.setItem('newTrainDestination', newTrainDestination);
+        // localStorage.setItem('newTrainFirstArrival', newTrainFirstArrival);
+        // localStorage.setItem('newTrainInterval', newTrainInterval);
 
-});
+        database.ref().push({
+            newTrain: newTrain,
+            newTrainDestination: newTrainDestination,
+            newTrainFirstArrival: newTrainFirstArrival,
+            newTrainInterval: newTrainInterval,
+            
+        });
+
+
+        database.ref().on('child_added', function (childSnapshot) {
+            let newTR = $('<tr>');
+            let trainName = $('<td>').text(childSnapshot.val().newTrain);
+            let trainDesination = $('<td>').text(childSnapshot.val().newTrainDestination);
+            let trainFrequency = $('<td>').text(childSnapshot.val().newTrainInterval);
+            let nextArrivalTime = $('<td>').text(nextArrival);
+            let nextArrivalInterval = $('<td>').text(timeRemaining);
+            newTR.append(trainName, trainDesination, trainFrequency, nextArrivalTime, nextArrivalInterval);
+            $('.trainInfo').append(newTR);
+
+        })
+    })
+
+    // database.ref('users').push(function)
+    // // change on click to push to firebase as a JSON object
+    // database.ref('users').on('child_added', function(snapshot){
+    // pull data from Fireadd data to page
+    // newTR.append(trainName,trainDesination,trainFrequency,nextArrivalTime,nextArrivalInterval);
+    //     $('.trainInfo').append(newTR);
+
+})
+
+// });
+
+//
+
